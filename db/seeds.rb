2d6a38f072899043d91a0e4a7fda6d5c949c0360
wbvg-mac <will@mylittleshowpony.com>
2013-09-05 23:06:22 +1000

    heroku mission

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,6 @@ u1 = User.create(:timer => '2013-09-02 08:24:00', :username => 'Willy', :gender 
 u2 = User.create( :timer => '2013-09-02 08:24:00', :username => 'Missey', :gender => 'Male', :birthday => '1982-12-21', :email => 'wbvg@nyxmyth.com', :is_admin => false, :time_zone => 'Sydney', :password => '12345', :password_confirmation => '12345')
 
 
-
-
 u1.save
 u2.save
 
